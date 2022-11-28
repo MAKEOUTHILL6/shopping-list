ce927f1c0b245f77269124e9b605aa0f52fc9477
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-11-28 20:55:29 +0200

    Items add modal and trpc routes implemented

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,8 @@
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
+import { itemsRouter } from "./itemsRouter";
 
 export const appRouter = router({
-  example: exampleRouter,
+  items: itemsRouter,
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/itemsRouter.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const itemsRouter = router({
+  addItem: publicProcedure
+    .input(z.object({ name: z.string(), checked: z.boolean() }))
+    .mutation(({ input, ctx }) => {
+        
+      const { name, checked } = input;
+
+      const item = ctx.prisma.shoppingItem.create({
+        data: {
+            name,
+            checked,
+        }
+      });
+
+      return item;
+
+    }),
+});
